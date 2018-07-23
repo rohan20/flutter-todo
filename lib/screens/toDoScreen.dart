bd4b6e25d5cfba8039cb2d1248b328a275d0644d
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-07-23 16:07:02 +0530

    added ListView to display dummy ToDos

--- lib/screens/toDoScreen.dart
+++ lib/screens/toDoScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_app/items/toDoItem.dart';
+import 'package:to_do_app/model/toDo.dart';
 
 class ToDoScreen extends StatefulWidget {
   @override
@@ -10,9 +12,7 @@ class _ToDoScreenState extends State<ToDoScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _appBar(),
-      body: Container(
-        color: Colors.white,
-      ),
+      body: toDoList(),
       floatingActionButton: _fab(),
     );
   }
@@ -21,6 +21,55 @@ class _ToDoScreenState extends State<ToDoScreen> {
     return AppBar(
       title: Text("Flutter To Do"),
     );
+  }
+
+  toDoList() {
+    return Container(
+      margin: const EdgeInsets.only(top: 12.0),
+      child: ListView.builder(
+        itemCount: _dummyToDoList().length,
+        itemBuilder: (context, index) {
+          return _buildToDoListItem(index);
+        },
+      ),
+    );
+  }
+
+  List<ToDo> _dummyToDoList() {
+    return <ToDo>[
+      ToDo(
+        "Buy eggs & milk",
+      ),
+      ToDo(
+        "Iron shirts",
+        true,
+      ),
+      ToDo(
+        "Buy eggs & milk",
+      ),
+      ToDo(
+        "Iron shirts",
+        true,
+      ),
+      ToDo(
+        "Buy eggs & milk",
+      ),
+      ToDo(
+        "Iron shirts",
+        true,
+      ),
+      ToDo(
+        "Buy eggs & milk",
+      ),
+      ToDo(
+        "Iron shirts",
+        true,
+      ),
+    ];
+  }
+
+  _buildToDoListItem(int index) {
+    return ToDoItem(_dummyToDoList()[index]);
   }
 
   FloatingActionButton _fab() {
