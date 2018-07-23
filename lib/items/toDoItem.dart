cc5ed326dae6a2dd618e84971cd5ac90c264be24
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-07-23 16:06:38 +0530

    added ToDo argument in constructor for toDoItem

--- lib/items/toDoItem.dart
+++ lib/items/toDoItem.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_app/model/toDo.dart';
 
 class ToDoItem extends StatelessWidget {
+
+  ToDo _toDo;
+
+  ToDoItem(this._toDo);
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.all(12.0),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         mainAxisSize: MainAxisSize.max,
         children: <Widget>[
-          Text("ToDo title"),
+          Text(
+            _toDo.title,
+            style: Theme.of(context).textTheme.headline,
+          ),
           Icon(
-            Icons.done,
+            _toDo.isChecked ? Icons.done : null,
             color: Colors.black,
           )
         ],
