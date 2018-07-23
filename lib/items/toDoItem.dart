8b153e07be326fe605e1d1bb0a2d554e99fb5f90
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-07-23 16:21:09 +0530

    added inkwell widget on list item for taps

--- lib/items/toDoItem.dart
+++ lib/items/toDoItem.dart
@@ -2,28 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:to_do_app/model/toDo.dart';
 
 class ToDoItem extends StatelessWidget {
-
   ToDo _toDo;
 
   ToDoItem(this._toDo);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(12.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        mainAxisSize: MainAxisSize.max,
-        children: <Widget>[
-          Text(
-            _toDo.title,
-            style: Theme.of(context).textTheme.headline,
-          ),
-          Icon(
-            _toDo.isChecked ? Icons.done : null,
-            color: Colors.black,
-          )
-        ],
+    return InkWell(
+      onTap: () {
+        print("Tapped");
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Text(
+              _toDo.title,
+              style: Theme.of(context).textTheme.headline,
+            ),
+            Icon(
+              _toDo.isChecked ? Icons.done : null,
+              color: Colors.black,
+            )
+          ],
+        ),
       ),
     );
   }
